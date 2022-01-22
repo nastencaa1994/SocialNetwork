0117cd25938711c0f4f3c463b5c05f3736fe4392
Bogdan A.D <76436777+nastencaa1994@users.noreply.github.com>
2022-01-22 17:33:18 +0300

    store

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import './index.css'
-import state, {subcribe} from "./redux/state";
+import store from "./redux/state";
 import {BrowserRouter} from "react-router-dom"
 import {render} from "react-dom"
 import App from "./App";
-import {addPost, newTextFunction} from "./redux/state";
+
 
 
 const contentElement = document.getElementById("root")
 
-export let rerenderEntireTree=(state)=>{
+export let rerenderEntireTree=()=>{
     render(
         <BrowserRouter>
-            < App state={state} addPost={addPost} newTextFunction={newTextFunction}/>
+            < App state={store.getState()} addPost={store.addPost.bind(store)} newTextFunction={store.newTextFunction.bind(store)}/>
         </BrowserRouter>
         , contentElement
     );
 }
-rerenderEntireTree(state)
-subcribe(rerenderEntireTree);
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree);
--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -1,51 +1,64 @@
 import React from "react";
 
-let rerenderEntireTree=()=>{
-    console.log('state change')
-}
 
-let state={
-    profilePage:{
-        myPost: [
-            {id: 1, text: "Hi, hoe are you?", countLike: 41},
-            {id: 2, text: "It's my first pos", countLike: 14}
-        ],
-        newPostText:"Введите текст"
 
+let store = {
+    _state: {
+        profilePage: {
+            myPost: [
+                {id: 1, text: "Hi, hoe are you?", countLike: 41},
+                {id: 2, text: "It's my first pos", countLike: 14}
+            ],
+            newPostText: "Введите текст"
+
+        },
+        dialogsPage: {
+            message: [
+                {id: 1, type: 'incoming', time: '23:52', text: "hello"},
+                {id: 2, type: 'incoming', time: '23:53', text: "How are you?"},
+                {id: 3, type: 'outgoing', time: '00:05', text: "hello"},
+                {id: 4, type: 'Outgoing', time: '00:06', text: "Fine"}
+            ],
+            dialogsData: [
+                {id: 1, name: 'Настя'},
+                {id: 2, name: 'Костя'},
+                {id: 3, name: 'Остап'},
+                {id: 4, name: 'Люся'},
+                {id: 5, name: 'Наташа'}
+            ]
+        },
+        sidebar: {}
     },
-    dialogsPage:{
-        message : [
-            {id: 1, type: 'incoming', time: '23:52', text: "hello"},
-            {id: 2, type: 'incoming', time: '23:53', text: "How are you?"},
-            {id: 3, type: 'outgoing', time: '00:05', text: "hello"},
-            {id: 4, type: 'Outgoing', time: '00:06', text: "Fine"}
-        ],
-        dialogsData : [
-            {id: 1, name: 'Настя'},
-            {id: 2, name: 'Костя'},
-            {id: 3, name: 'Остап'},
-            {id: 4, name: 'Люся'},
-            {id: 5, name: 'Наташа'}
-        ]
-    },
-    sidebar:{}
-}
 
-export const addPost= () => {
-    let newPost = {
-        id:3,
-        text:  state.profilePage.newPostText,
-        countLike:0
+    getState(){
+        return this._state
+    },
+
+    //метод перезагрузки С заглушкой
+    _callSubscriber() {
+        console.log('state change')
+    },
+    //метод добавления поста
+    addPost() {
+        let newPost = {
+            id: 3,
+            text: this._state.profilePage.newPostText,
+            countLike: 0
+        }
+        this._state.profilePage.myPost.push(newPost);
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+    //метод отслеживание, изменение и перерисовка текста в поле новый пост
+    newTextFunction (text){
+        this._state.profilePage.newPostText = text;
+        this._callSubscriber(this._state)
+    },
+
+    subscribe (observer)  {
+        this._callSubscriber = observer;
     }
-   state.profilePage.myPost.push(newPost);
-    state.profilePage.newPostText=''
-    rerenderEntireTree(state)
 }
-export const newTextFunction = (text) => {
-    state.profilePage.newPostText=text;
-    rerenderEntireTree(state)
-}
-export const subcribe = (observer)=>{
-    rerenderEntireTree = observer;
-}
-    export default state
+
+
+export default store
