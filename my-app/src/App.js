2008f9c7e41253988ae17e2a261f5f9bce94a277
Bogdan A.D <76436777+nastencaa1994@users.noreply.github.com>
2023-02-16 10:22:07 +0300

    test_branch_save

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Setting from "./components/Setting/Setting";
 
-
+//просто тестирую слияние веток
 const App = (props) => {
     return (
         <div className='app-wrapper'>
